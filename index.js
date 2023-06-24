1e2be5e65c52bc56d8b3617bac8a5d0d6d64b1dc
sam <syy31@cam.ac.uk>
2023-06-24 16:35:19 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('Welcome to the calculator!');
+const readline = require('readline-sync');
 
+console.log('Welcome to the calculator!');
+console.log('Please enter a number: ');
+const num1 = readline.prompt();
+console.log('Please enter a second number: ');
+const num2 = readline.prompt();
+let product = +num1 * +num2;
+console.log("The product of %o and %o is: %o", num1, num2, product);
