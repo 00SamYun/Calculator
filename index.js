2d4596015efc957e24ff6cf684167c9ab4d0a17a
sam <syy31@cam.ac.uk>
2023-06-24 16:51:38 +0100

    Changed the string formatting

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const num1 = readline.prompt();
 console.log('Please enter a second number: ');
 const num2 = readline.prompt();
 let product = +num1 * +num2;
-console.log("The product of %o and %o is: %o", num1, num2, product);
+console.log(`The product of ${num1} and ${num2} is ${product}`);
