ef06f2666347cbf75ad8126393baef46859ab5a7
sam <syy31@cam.ac.uk>
2023-06-24 20:12:12 +0100

    Refactoring and input validation

--- index.js
+++ index.js
@@ -1,45 +1,88 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
-console.log('==========================');
-
-console.log('Please enter the operator: ');
-const op = readline.prompt();
-
-console.log('How many numbers do you want to perform this operation on? ')
-const n = readline.prompt();
-const all_nums = Array(+n);
-
-for (i = 0; i < n; i++){
-    console.log(`Please enter number ${i+1}: `);
-    all_nums[i] = +readline.prompt();
+printWelcomeMessage();
+let stop = 'y';
+while (stop == 'y'){
+    const op = getOperator();
+    const n = getN();
+    const result = performOneCalculation(getOperands(n), op);
+    console.log('The answer is: ' + result);
+    stop = getResponse('Press y to do another calculation. Press any other key to exit.');
 }
 
-let result;
 
-switch (op){
-    case '+':
-        result = all_nums.reduce(
-            (accumulator, currentValue) => accumulator + currentValue,
-        );
-        break;
-    case '-':
-        result = all_nums.reduce(
-            (accumulator, currentValue) => accumulator - currentValue,
-        );
-        break;
-    case '*':
-        result = all_nums.reduce(
-            (accumulator, currentValue) => accumulator * currentValue,
-        );
-        break;
-    case '/':
-        result = all_nums.reduce(
-            (accumulator, currentValue) => accumulator / currentValue,
-        );
-        break;
+function printWelcomeMessage(){
+    console.log('Welcome to the calculator!');
+    console.log('==========================');
+}
+function getResponse(text){
+    console.log(text);
+    const response = readline.prompt();
+    return response;
+};
+
+function getNumber(text){
+    let number;
+    do {
+        number = +getResponse(text);
+    } while (isNaN(number));
+    return number;
 }
 
-console.log('The answer is: ' + result);
+function getN(){
+    const n = getNumber('How many numbers do you want to perform this operation on? ');
+    if (n <= 1){
+        console.log('Please choose a number greater than 1.');
+        getN();
+    }
+    return n;
+}
+
+function getOperator(){
+    const valid_ops = ['+','-','*','/'];
+    const op = getResponse('Please enter the operator: ');
+    if (!(valid_ops.includes(op))){
+        console.log(`"${op}" is not a valid operator.`);
+        getOperator();
+    }
+    return op;
+}
+
+function getOperands(n){
+    const all_nums = [];
+    for (i = 0; i < n; i++){
+        all_nums[i] = getNumber(`Please enter number ${i+1}: `);
+    }
+    return all_nums;
+}
+
+function performOneCalculation(operands, operator){
+    let result;
+    switch (operator){
+        case '+':
+            result = operands.reduce(
+                (accumulator, currentValue) => accumulator + currentValue,
+            );
+            break;
+        case '-':
+            result = operands.reduce(
+                (accumulator, currentValue) => accumulator - currentValue,
+            );
+            break;
+        case '*':
+            result = operands.reduce(
+                (accumulator, currentValue) => accumulator * currentValue,
+            );
+            break;
+        case '/':
+            result = operands.reduce(
+                (accumulator, currentValue) => accumulator / currentValue,
+            );
+            break;
+    }
+    return result;
+}
+
+
 
 
