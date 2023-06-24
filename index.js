7ed586c2cff3464902060c6b7c9ffeb5eee9fd7a
sam <syy31@cam.ac.uk>
2023-06-24 17:00:56 +0100

    Branching with if...else

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
+console.log('==========================');
+console.log('Please enter an operator: ');
+const op = readline.prompt();
 console.log('Please enter a number: ');
 const num1 = readline.prompt();
 console.log('Please enter a second number: ');
 const num2 = readline.prompt();
-let product = +num1 * +num2;
-console.log(`The product of ${num1} and ${num2} is ${product}`);
+let result;
+
+if (op == '+'){
+    result = +num1 + +num2;
+} else if (op == '-'){
+    result = +num1 - +num2;
+} else if (op == '*'){
+    result = +num1 * +num2;
+} else if (op == '/'){
+    result = +num1 / +num2;
+}
+
+console.log(`${num1} ${op} ${num2} = ${result}`)
